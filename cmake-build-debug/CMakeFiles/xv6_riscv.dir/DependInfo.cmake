1d83137b223dc38fe6d179f7a686238ac7215a25
Mahdikhoshzat <mahdikhoshzat2003@gmail.com>
2024-03-23 13:35:19 +0430

    setup

---
+++ cmake-build-debug/CMakeFiles/xv6_riscv.dir/DependInfo.cmake
@@ -0,0 +1,73 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/bio.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/bio.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/console.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/console.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/exec.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/exec.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/file.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/file.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/fs.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/fs.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/kalloc.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/kalloc.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/log.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/log.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/main.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/main.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/pipe.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/pipe.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/plic.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/plic.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/printf.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/printf.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/proc.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/proc.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/ramdisk.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/ramdisk.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/sleeplock.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/sleeplock.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/spinlock.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/spinlock.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/start.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/start.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/string.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/string.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/syscall.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/syscall.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/sysfile.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/sysfile.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/sysproc.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/sysproc.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/trap.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/trap.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/uart.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/uart.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/virtio_disk.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/virtio_disk.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/kernel/vm.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/kernel/vm.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/mkfs/mkfs.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/mkfs/mkfs.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/cat.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/cat.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/echo.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/echo.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/forktest.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/forktest.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/grep.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/grep.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/grind.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/grind.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/init.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/init.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/kill.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/kill.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/ln.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/ln.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/ls.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/ls.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/mkdir.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/mkdir.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/printf.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/printf.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/rm.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/rm.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/sh.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/sh.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/stressfs.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/stressfs.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/ulib.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/ulib.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/umalloc.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/umalloc.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/usertests.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/usertests.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/wc.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/wc.c.obj"
+  "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/user/zombie.c" "C:/Users/LENOVO/Desktop/osProject/xv6-riscv/xv6-riscv/cmake-build-debug/CMakeFiles/xv6_riscv.dir/user/zombie.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../kernel"
+  "../user"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
